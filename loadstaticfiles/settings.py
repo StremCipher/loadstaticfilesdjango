8943637205f9237dd5ee5dc16da9c22a52aab9b3
StremCipher <47041923+StremCipher@users.noreply.github.com>
2021-10-18 19:42:53 +0530

    ok

--- loadstaticfiles/settings.py
+++ loadstaticfiles/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'loadstaticfiles.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        'NAME': str(os.path.join(BASE_DIR, 'db.sqlite3'))
     }
 }
 # it will work on heroku but satatic files will stop loding
