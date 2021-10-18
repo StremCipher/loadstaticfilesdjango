e4e957e04145ba41e83cca90e91cdf07fc73bb8a
StremCipher <47041923+StremCipher@users.noreply.github.com>
2021-10-18 20:53:26 +0530

    ok

--- loadstaticfiles/settings.py
+++ loadstaticfiles/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ufjxv#)h%!x0w064z*vz+1t9n-f6w!u@l1il5axmj313m7k4u('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['netutil.herokuapp.com', '127.0.0.1']
 
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
